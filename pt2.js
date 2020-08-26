f6d19dda52458674d84823c14d2ac7821a6876b7
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2020-08-26 08:53:29 +0200

    clean up

--- pt2.js
+++ pt2.js
@@ -1,36 +1,5 @@
 const input = [19, 2, 42, 18]
 
-function findMax(arr) {
-    let max = arr[0]
-    let maxIndex = 0
-    for (i in arr) {
-        if (arr[i] > max) {
-            maxIndex = i
-            max = arr[i]
-        }
-    }
-    return maxIndex
-}
-
-function sumMax(input) {
-    let indexOfMax = findMax(input)
-    let max = input[indexOfMax]
-    let result = 0
-    input.forEach((num, index) => {
-        if (index !== indexOfMax) {
-            let sum = num + max
-            if (sum % 2 != 0 && sum > result) {
-                result = sum
-            }
-        }
-    })
-    return result
-}
-
-console.log(sumMax(input))
-
-// alternative (improvement)
-
 function sumMaxAlt(input) {
     let evenMax = 0
     let oddMax = 0
